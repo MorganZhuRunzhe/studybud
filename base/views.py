f79982d106f5fa365dc2a6b791f92940807e41ae
Zhu Runzhe(Morgan) <MorganZhuRunzhe@gmail.com>
2023-09-12 22:21:43 +1000

    Add user login and logout feature

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -10,6 +13,38 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Git from Zero to Hero'},
 #     {'id': 3, 'name': 'Django from Zero to Hero'},
 # ]
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+        print(user)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+    context = {}
+
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    
+    logout(request)
+
+    return redirect('home')
+
 
 def home(request):
     # rooms = Room.objects.all()
@@ -26,9 +61,6 @@ def home(request):
     room_count = rooms.count()
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
-    return render(request, 'base/home.html', context)
-
-    context ={'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
