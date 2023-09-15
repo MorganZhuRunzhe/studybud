48cfb7514a72760d3237833d2a9cca64f44cb7b7
Zhu Runzhe(Morgan) <MorganZhuRunzhe@gmail.com>
2023-09-15 15:05:57 +1000

    Add mobile responsiveness

--- base/views.py
+++ base/views.py
@@ -85,7 +85,7 @@ def home(request):
         )
     # query upwards with "__" i.e. topics' name that room belongs to 
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
 
     # room_messages = Message.objects.all()
@@ -228,3 +228,20 @@ def updateUser(request):
     context = {'form': form}
 
     return render(request, 'base/update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+
+    context = {'topics':topics}
+    
+    return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+
+    context = {'room_messages': room_messages}
+
+    return render(request, 'base/activity.html', context)
